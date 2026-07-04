4e3a37d08faf1d369f6582c21c6fe735733c7fb0
kimbosung521 <sorisl37@naver.com>
2026-07-04 19:13:46 +0900

    feat:expo-14 관리자 로그인 화면

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,19 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
-// NativeWind가 global.css(Tailwind 디렉티브)를 Metro 변환 파이프라인에 주입하도록 래핑합니다.
-module.exports = withNativeWind(mergeConfig(getDefaultConfig(__dirname), config), {
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer/react-native'),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+module.exports = withNativeWind(mergeConfig(defaultConfig, config), {
   input: './global.css',
 });
