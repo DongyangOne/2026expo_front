a731e5b890204c193ad094cd0684ab5673a1363d
kawon-lee <boj171124@gmail.com>
2026-07-05 18:01:12 +0900

    상단바 보호코드 추가

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
 const SearchScreen = () => {
   return (
-    <View className="flex-1">
+    <SafeAreaView className="flex-1">
       <WebView source={{ uri: 'https://xn--oy2b29bd3a601b.kr//' }} className="flex-1"></WebView>
-    </View>
+    </SafeAreaView>
   );
 };
 
